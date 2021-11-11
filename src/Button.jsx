f52bf181351dcd438452a65d33b4182e7d0ec138
Peter Sviryd <Peter.blr@yandex.ru>
2021-11-11 12:49:27 +0300

    const Button

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,21 +1,42 @@
 import React from "react";
 import classNames from "classnames";
 
-class Button extends React.Component {    
+const Button = (props) => {
+  return (
+    <button
+      className={classNames("button", {
+        "button--outline": props.outline,
+      })}
+    >
+      {props.children}
+    </button>
+  );
+};
 
-    render() {
-        //return <button className={`button ${this.props.outline ? "button--outline" : ""}`}>{this.props.children}</button>
-        return <button className={classNames(
-            "button",
-            {
-            "button--outline": this.props.outline,
-            },
-            {
-                "button--outline": this.props.outline,
-            },
-        )}>{this.props.children}</button>
-    }
-}
+// function Button(props) {
+//   return (
+//     <button
+//       className={classNames("button", {
+//         "button--outline": this.props.outline,
+//       })}
+//     >
+//       {this.props.children}
+//     </button>
+//   );
+// }
+
+//class Button extends React.Component {
+
+//     render() {
+//         //return <button className={`button ${this.props.outline ? "button--outline" : ""}`}>{this.props.children}</button>
+//         return <button className={classNames(
+//             "button",
+//             {
+//             "button--outline": this.props.outline,
+//             },
+//         )}>{this.props.children}</button>
+//     }
+// }
 
 // class Button extends React.Component {
 //     render() {
