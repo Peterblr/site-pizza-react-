78584fcaa6b83c7f6501a861f6e507380c670b04
Peter Sviryd <Peter.blr@yandex.ru>
2021-11-11 23:00:04 +0300

    add key by map

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -5,8 +5,8 @@ function Categories({ items }) {
     <div className="categories">
       <ul>
         <li className="active">Все</li>
-        {items.map((name) => (
-          <li>{name}</li>
+        {items.map((name, index) => (
+          <li key={`${name}_${index}`}>{name}</li>
         ))}
       </ul>
     </div>
