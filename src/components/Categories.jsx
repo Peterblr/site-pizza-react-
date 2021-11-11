b0a3a72ba4c749ca0664aaaddfa407457d947f8c
Peter Sviryd <Peter.blr@yandex.ru>
2021-11-11 23:29:20 +0300

    active by this.setState

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 
 class Categories extends React.Component {
+  state = {
+    activeItem: null,
+  };
+
+  onSelectItem = (index) => {
+    this.setState({ activeItem: index });
+  };
+
   render() {
     const { items, onClickItem } = this.props;
     return (
@@ -8,7 +16,11 @@ class Categories extends React.Component {
         <ul>
           <li className="active">Все</li>
           {items.map((name, index) => (
-            <li onClick={() => onClickItem(name)} key={`${name}_${index}`}>
+            <li
+              className={this.state.activeItem === index ? "active" : ""}
+              onClick={() => this.onSelectItem(index)}
+              key={`${name}_${index}`}
+            >
               {name}
             </li>
           ))}
